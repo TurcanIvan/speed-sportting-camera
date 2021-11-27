364e18dabc412bd2e31157c98738c92a0e408d9b
TurcanIvan <68420218+TurcanIvan@users.noreply.github.com>
2021-11-27 08:32:32 +0200

    Exemple

---
+++ speed-sportting-camera.py
@@ -0,0 +1,11 @@
+car_speed   = 150      # km/h
+SPEED_LIMIT = 70       # km/h
+
+if car_speed > SPEED_LIMIT:
+    print ("The driver will be punisched wif a fee of 50 U.C. ")
+
+print("Joob done!!!")
+    ##########################################
+
+    # [input data] ------> < car_speed > SPPED_LIMIT??? ---> True ---> "Punished" --->"Done !"
+    #                                                    \-> False ----------------------^  
